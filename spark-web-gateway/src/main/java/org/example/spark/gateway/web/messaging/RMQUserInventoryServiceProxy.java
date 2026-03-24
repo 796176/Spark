089c1bad3a1e8d68bfd83c5b6cfa0ecb96fd59d0
796176 <150280490+796176@users.noreply.github.com>
2026-03-24 13:02:14 +0300

    Implement the messaging for the inventory related commands

---
+++ spark-web-gateway/src/main/java/org/example/spark/gateway/web/messaging/RMQUserInventoryServiceProxy.java
@@ -0,0 +1,77 @@
+/*
+ * Spark - The inventory management application
+ * Copyright (C) 2026 Yegore Vlussove
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.example.spark.gateway.web.messaging;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import jakarta.annotation.Nonnull;
+import org.example.spark.authorization.Role;
+import org.example.spark.gateway.web.converters.InventoryServiceCommandEncoder;
+import org.example.spark.gateway.web.converters.RoleEncoder;
+import org.example.spark.gateway.web.models.RemoteCallResult;
+import org.example.spark.gateway.web.proxies.UserInventoryServiceProxy;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+public class RMQUserInventoryServiceProxy implements UserInventoryServiceProxy {
+
+	private final Channel channel;
+
+	private final RMQConsumer rmqConsumer;
+
+	private final InventoryServiceCommandEncoder inventoryServiceCommandEncoder;
+
+	public RMQUserInventoryServiceProxy(
+		@Nonnull Channel channel,
+		@Nonnull RMQConsumer rmqConsumer,
+		@Nonnull InventoryServiceCommandEncoder inventoryServiceCommandEncoder
+	) {
+		this.channel = channel;
+		this.rmqConsumer = rmqConsumer;
+		this.inventoryServiceCommandEncoder = inventoryServiceCommandEncoder;
+	}
+
+	@Override
+	public void getInventory(
+		long callerId, @Nonnull Role[] roles, @Nonnull Consumer<RemoteCallResult> callResultConsumer
+	) throws Exception {
+		String correlationId = UUID.randomUUID().toString();
+		rmqConsumer.register(correlationId, callResultConsumer);
+		InventoryServiceCommandEncoder.EncodedCommand encodedCommand =
+			inventoryServiceCommandEncoder.encodeGettingItemsCommand();
+		String encodedRoles = RoleEncoder.encode(roles);
+		AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
+			.deliveryMode(2)
+			.correlationId(correlationId)
+			.contentType(encodedCommand.contentType())
+			.type("org.example.spark.inventory.get-items")
+			.headers(Map.of(
+				"Caller-Id", Long.toString(callerId),
+				"Version", encodedCommand.version(),
+				"Caller-Roles", encodedRoles
+			))
+			.messageId(UUID.randomUUID().toString())
+			.replyTo("spark-web-gateway")
+			.build();
+		channel.basicPublish("commands", "spark-inventory-service", properties, encodedCommand.body());
+		channel.waitForConfirms();
+	}
+}
