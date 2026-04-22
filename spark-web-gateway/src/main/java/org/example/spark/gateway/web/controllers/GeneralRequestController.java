2a63d12843a249b5e6cdbb5ff1d3e15792f4448a
796176 <150280490+796176@users.noreply.github.com>
2026-04-22 16:02:51 +0300

    Add HTTP access points for 4xx

--- spark-web-gateway/src/main/java/org/example/spark/gateway/web/controllers/GeneralRequestController.java
+++ spark-web-gateway/src/main/java/org/example/spark/gateway/web/controllers/GeneralRequestController.java
@@ -50,6 +50,16 @@ public class GeneralRequestController {
 		};
 	}
 
+	@GetMapping("/400")
+	public String error400() {
+		return "400";
+	}
+
+	@GetMapping("/403")
+	public String error403() {
+		return "403";
+	}
+
 	@GetMapping("/500")
 	public String error500() {
 		return "500";
